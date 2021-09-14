a9ee6ea3b3f4330d6dfb638d1638d481895ee40c
Gaurav Prakash Pareek <gppareek22@gmail.com>
2021-09-14 21:20:22 +0530

    Files Updated

--- titanic_review/titanic_review/views.py
+++ titanic_review/titanic_review/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
+def result(request):
+    p_id=request.GET['passenger_id']
+    print(p_id)
+    return render(request,'result.html')
